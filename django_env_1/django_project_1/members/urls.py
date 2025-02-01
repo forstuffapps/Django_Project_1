ed9c093bbd94b08d00e9ffce90ea5e8de8a5d86d
forstuffapps <forstuffapps@gmail.com>
2025-02-01 17:32:33 -0500

    django templates - added more pages and master templates

--- django_env_1/django_project_1/members/urls.py
+++ django_env_1/django_project_1/members/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns =[
     path('members/', views.members, name="members"),
+    path('details/<int:id>', views.details, name='details'),
 ]
--- django_env_1/django_project_1/members/views.py
+++ django_env_1/django_project_1/members/views.py
@@ -17,4 +17,11 @@ def members(request):
 
 
 
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember' : mymember,
+    }
 
+    return HttpResponse(template.render(context, request))
